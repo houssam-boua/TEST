9b6d5730c62115d44b247f4ae10e7c575c8b4ec1
idrisselhouari <idrisselhouarii@gmail.com>
2025-05-17 19:05:09 +0100

    add document done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,6 +16,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+BACKEND_DIR = "backend/media/"
 
 # Define the custom user model
 AUTH_USER_MODEL = "users.User"
